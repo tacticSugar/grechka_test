70358a46248f7224bf3fb0a9df9f176174f3acf6
Vladimir <balaian.vladimir@yandex.ru>
2022-10-07 02:44:48 +0300

    fixed fetch, finish basic layout

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,22 +1,23 @@
 import { useState, useEffect } from 'react'
 import Card from '../Card/Card'
+import styles from './CardList.module.scss'
 
 function CardList() {
   const [films, setFilms] = useState([])
-  const url = 'https://run.mocky.io/v3/59f47e8e-2a09-48c3-8a1d-0af8e5817f7c'
+  const url = 'https://run.mocky.io/v3/f41356c2-e1ee-4fe3-aad7-62e2c5bb68a4'
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(url)
       const data = await response.json()
-      console.log(JSON.parse(data))
-      setFilms(JSON.parse(data))
+      const dataString = JSON.stringify(data.data)
+      setFilms(JSON.parse(dataString))
     }
 
     fetchData()
   }, [])
 
   return (
-    <>
+    <div className={styles.container}>
       {films.map((film) => {
         return (
           <Card
@@ -24,10 +25,11 @@ function CardList() {
             src={film.src}
             name={film.name}
             description={film.description}
+            key={film.id}
           />
         )
       })}
-    </>
+    </div>
   )
 }
 
